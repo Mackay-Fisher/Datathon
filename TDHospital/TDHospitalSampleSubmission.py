5a323a6b142e6872d88320b8d6258f6a1ca5acef
Mackay-Fisher <Mackeyfisher@gmail.com>
2023-10-28 18:48:34 -0500

    chagi you might be illiterate

--- TDHospital/TDHospitalSampleSubmission.py
+++ TDHospital/TDHospitalSampleSubmission.py
@@ -34,23 +34,4 @@ class Solution:
 
 
 # BOILERPLATE
-@app.route("/death_probability", methods=["POST"])
-def q1():
-    solution = Solution()
-    data = request.get_json()
-    return {
-        "probability": solution.calculate_death_prob(data['timeknown'], data['cost'], data['reflex'], data['sex'], data['blood'],
-                                            data['bloodchem1'], data['bloodchem2'], data['temperature'], data['race'],
-                                            data['heart'], data['psych1'], data['glucose'], data['psych2'],
-                                            data['dose'], data['psych3'], data['bp'], data['bloodchem3'],
-                                            data['confidence'], data['bloodchem4'], data['comorbidity'],
-                                            data['totalcost'], data['breathing'], data['age'], data['sleep'],
-                                            data['dnr'], data['bloodchem5'], data['pdeath'], data['meals'],
-                                            data['pain'], data['primary'], data['psych4'], data['disability'],
-                                            data['administratorcost'], data['urine'], data['diabetes'], data['income'],
-                                            data['extraprimary'], data['bloodchem6'], data['education'], data['psych5'],
-                                            data['psych6'], data['information'], data['cancer'])}
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5555)
